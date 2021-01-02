54d8e6be9da36d7c2b4e6f1bf25307c3d2efd81d
AndyDev25 <itadrian.b@gmail.com>
2021-01-02 03:28:26 +0100

    Added Diary component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Diary class="min-h-screen" />
+</template>
+
+<script>
+import Diary from "./components/Diary.vue";
+
+export default {
+  name: "App",
+  components: {
+    Diary
+  }
+};
+</script>
